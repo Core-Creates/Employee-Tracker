5e62287bbc036f4f0ee41493335439d8849a7919
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-04-20 00:17:33 -0500

    added sequelize npm library and models to the project

---
+++ models/Employee.js
@@ -0,0 +1,67 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Employee extends Model { }
+
+Employee.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newEmployee) => {
+                try {
+                    
+                    return newEmployee;
+                } catch (err) {
+                    console.log(err);
+                    return err;
+                }
+            },
+            beforeUpdate: async (updatedEmployee) => {
+                try {
+                    
+                    return updatedEmployee;
+                } catch (err) {
+                    console.log(err);
+                    return err;
+                }
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'reader',
+    }
+);
+
+module.exports = Employee;
